ba79551b8372ccb47198dc8633f930a096c93ff8
Mynor Morales <mynormh@outlook.com>
2018-12-18 20:56:37 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '@babel/polyfill';
+import App from './App';
+import DefaultErroBoundary from './DefaultErrorBoundary';
+import './styles.css';
+
+if (process.env.NODE_ENV === 'development') {
+  const axe = require('react-axe');
+  axe(React, ReactDOM, 1000);
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <DefaultErroBoundary>
+      <App />
+    </DefaultErroBoundary>
+  </React.StrictMode>,
+  document.getElementById('app')
+);
